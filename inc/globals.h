85052abc2bc3f8e0fc748169d0f338b0df535a1f
specul8tor <azaanazam1606@gmail.com>
2023-04-10 16:03:56 -0600

    initial motor spinning

--- inc/globals.h
+++ inc/globals.h
@@ -4,6 +4,7 @@
 #include <SystemClock.h>
 #include <Structs.h>
 
+#define INIT
 // #define DEV
 #define VERBOSE
 // #define CONFIG_FILE
@@ -22,14 +23,28 @@ typedef struct PWM {
 
 #define DEFAULT_IO_CFG IO_FAST_SLEW|IO_PULL_UP|IO_PULL_ENABLE|IO_DRIVE_4mA
 
-#define led1_pwm   { LED1, 500 }
-#define led2_pwm   { LED2, 500 }
-
 #define LED1 { GPIO_A, io_PA, 0x1, Polar_ActiveHigh }
 #define LED2 { GPIO_A, io_PA, 0x2, Polar_ActiveHigh }
 
+#define led1_pwm   { LED1, 500 }
+#define led2_pwm   { LED2, 500 }
+
 uint8_t num_leds = 2;
 static PWM all_leds[] = { led1_pwm, led2_pwm };
+
+typedef struct MOTOR_DSHOT {
+  GlobalPin motor;
+  uint16_t throttle;
+} MOTOR_DSHOT;
+
+#define NUM_MOTORS 4
+
+#define MOTOR1 { GPIO_D, io_PD4, 0x10, Polar_ActiveHigh }
+#define MOTOR2 { GPIO_D, io_PD5, 0x20, Polar_ActiveHigh }
+#define MOTOR3 { GPIO_D, io_PD6, 0x30, Polar_ActiveHigh }
+#define MOTOR4 { GPIO_D, io_PD7, 0x40, Polar_ActiveHigh }
+
+MOTOR_DSHOT motors[NUM_MOTORS] = { { MOTOR1, 682}, { MOTOR2, 48}, { MOTOR3, 2047}, { MOTOR4, 0} };//, MOTOR2, MOTOR3, MOTOR4};
 
 int16_t acce_gyro_data[12];
 
--- inc/init.h
+++ inc/init.h
@@ -2,6 +2,7 @@
 #include <Thread.h>
 #include "globals.h"
 #include "utils.h"
+#include "esc.h"
 
 static inline void init_clock(void)
 {
@@ -17,15 +18,37 @@ static void led_init()
     return;
 }
 
+static void motor_init()
+{
+    for (size_t i = 0; i < NUM_MOTORS; i++){
+      io_set_config(DEFAULT_IO_CFG, motors[i].motor.io_port);
+      globalPin_set_dir(PinDir_Output, &motors[i].motor);
+    }
+    return;
+}
+
+static void* motor_thread(void* ptr){
+  MOTOR_DSHOT * motor = (MOTOR_DSHOT *)ptr;
+  while(1){
+    send_dshot(motor->throttle, &motor->motor);
+  }
+}
+
 static void init(){
 
     xpd_puts("\nHello World\n");
     wait_ms(20);
     init_clock();
     led_init();
+    motor_init();
 
     globalPin_write(ON, &all_leds[0].pwm_out);
     blink_leds(1, 4, 500);
     globalPin_write(ON, &all_leds[1].pwm_out);
+    
+    for(size_t i = 0; i < NUM_MOTORS; i++){
+      thread_setup(motor_thread, &motors[i],1+i);
+      thread_run(1+i);
+    }
 
 }
